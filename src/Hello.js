188afe4ab7954ca85558b1cab46e528721ab8e6b
dimple sharma <dimplessharma@hotmail.com>
2020-07-24 11:53:03 -0600

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Hello extends React.Component{
+    render(){
+        return(
+            <>
+            <h2> My first Component</h2>
+            <p>Hello World!!</p>
+            </>
+
+        )
+    }
+    
+}
+export default Hello;
